68918f68310d026fa932e9eaaa4c84e3408130b0
baumgast <baumsen@gmail.com>
2017-11-08 17:31:52 +0100

    Added benchmarking results and plots to fig 3
    added benchmarking data and results of fitting benchmarking data, plot for benchmarking model selection, boxplots for parameter recovery not included yet.
    
    Individual particles compared to data as in fig3 B and appendix S3

--- plot_settings.py
+++ plot_settings.py
@@ -28,22 +28,101 @@ alpha_bg = 0.025
 
 line_wdth = 3
 
-rr = np.arange(0,125+1,5)
-win = np.zeros((len(rr),2))
-win[:,0] = rr
-win[:,1] = rr+125
+# ACF window positions for steady state data
+rr = np.arange(0, 125 + 1, 5)
+win = np.zeros((len(rr), 2))
+win[:, 0] = rr
+win[:, 1] = rr + 125
 
 
 # helper functions
-def acf_sliding_window(data,windows = win):
+def calc_histogram(data):
+    bins = np.logspace(0, 5, 50)
+    n = data.shape[0] * data.shape[1]
+    hist = np.histogram(data.reshape(n, ), bins=bins)
+    Hist = np.array(hist[0], dtype=float)
+    return bins[0:len(bins) - 1], Hist / n
+
+
+def AutocorrelateAllData(DataTC, deltaT=1):
+    res = np.empty((DataTC.shape))
+    count = 0
+    while count < DataTC.shape[1]:
+        if np.sum(DataTC[:, count] == 0) == DataTC.shape[0]:
+            rr = np.zeros(res.shape[0]) + 10
+            res[:, count] = rr
+        else:
+            res[:, count] = AutoCorrelate(DataTC[:, count], deltaT)[1]
+        count = count + 1
+    return res
+
+
+def AutoCorrelate(signal, deltaT):
+    signal = signal - np.mean(signal)
+    res = np.correlate(signal, signal, mode='full')
+    res = res / np.max(res)
+    out = np.empty((2, len(signal)))
+    out[0] = np.arange(0, len(signal)) * deltaT
+
+    out[1] = res[np.argmax(res):]
+    return out
+
+
+def acf_sliding_window(data, windows=win):
     TS = []
     ACF = []
     for ww in windows:
-        dd = data[int(ww[0]):int(ww[1]),:]
-        acf = AutocorrelateAllData(dd,1)
+        dd = data[int(ww[0]):int(ww[1]), :]
+        acf = AutocorrelateAllData(dd, 1)
         TS.append(dd)
         ACF.append(acf)
-    return TS,ACF
+    return TS, ACF
+
+
+def average_acf(ACF):
+    res = np.zeros((len(ACF), ACF[0].shape[0]))
+    for ii, acf in enumerate(ACF):
+        res[ii] = acf.mean(axis=1)
+    return res
+
+
+def bootstrap_acf_hl(acf, bins):
+    acf_hl_hist = np.zeros((len(acf), len(bins) - 1))
+    for ii in np.arange(0, len(acf)):
+        hl = ACF_HL_multi(acf[ii])
+        hist = np.histogram(hl, bins, normed=1)[0]
+        acf_hl_hist[ii] = hist
+    return acf_hl_hist
+
+
+def bootstrap_acf_lag(acf, bins):
+    acf_lag_hist = np.zeros((len(acf), len(bins) - 1))
+    for ii in np.arange(0, len(acf)):
+        lag = acf[ii][1, :]
+        hist = np.histogram(lag, bins, normed=1)[0]
+        acf_lag_hist[ii] = hist
+    return acf_lag_hist
+
+
+def ACF_HL(acf):
+    ind_below = np.where(acf <= 0.5)[0]
+    x_low = ind_below[0]
+    x_high = ind_below[0] - 1
+    acf_low = acf[x_low]
+    acf_high = acf[x_high]
+
+    slope = acf_low - acf_high
+    inter = acf_low - slope * x_low
+    hl = (0.5 - inter) / slope
+    return hl
+
+
+def ACF_HL_multi(ACF):
+    res = np.zeros(ACF.shape[1])
+    index = np.arange(0, ACF.shape[1])
+    for ii in index:
+        res[ii] = ACF_HL(ACF[:, ii])
+    return res
 
 
 def nice_boxplots(bp):
